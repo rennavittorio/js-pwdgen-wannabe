c779aeae0132fa67052dc696a3592de729d0ec7e
rennavittorio <renna.vittorio@gmail.com>
2023-01-30 14:49:32 +0100

    ADD algorithm structure set in human words

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//Esercizio JS
+
+// 1. Chiedere nome all'utente
+    //1.1 Dichiarare var firstName
+    //1.2 Assegnare a var valore richiesto ed immesso da utente
+
+// 2. Chiedere cognome all'utente
+    //2.1 Dichiarare var firstName
+    //2.2 Assegnare a var valore richiesto ed immesso da utente
+
+// 3. Chiedere colore preferito all'utente
+    //3.1 Dichiarare var firstName
+    //3.2 Assegnare a var valore richiesto ed immesso da utente
+
+// 4. Chiedere un numero all'utente
+    //4.1 Dichiarare var firstName
+    //4.2 Assegnare a var valore richiesto ed immesso da utente
+
+//5. Mostrare sulla pagina il risultato della concatenazione di: <nome><cognome><colorePreferito><numero> (es. GianlucaLomarcoRosso108)
+    // 5.1 Dichiarare var newPswUser
+    // 5.2 Assegnare a var valori concatenati inseriti da utente
+    // 5.3 Inviare nuovo valore a schermo
+
+//N.B. Dichiarare una var e assegnare valore possono essere svolte assieme (i.e. 'inizializzare')
